80502e778e316bb555afceebf31ef38abc7ad6ca
ChanceCafferty <chance.r.cafferty@gmail.com>
2022-03-25 13:22:57 -0500

    completed mvp objectives

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+import CharacterCard from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,9 +11,26 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/people/`)
+    .then(res => {
+      console.log(res.data[0].name)
+      console.log(res.data[1].name)
+      console.log(res.data[2].name)
+      console.log(res.data[3].name)
+      console.log(res.data[4].name)
+      console.log(res.data[5].name) 
+      setData(res.data);
+    })
+    .catch(err => console.error(err))
+  }, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data && <CharacterCard character={data} /> }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,25 @@
+import React from "react";
+import styled from "styled-components";
 // Write your Character component here
+const Char = styled.p`
+    border 10px solid blue;
+    background-color: yellow;
+    color: red;
+    padding 20px;
+    
+`
+
+const CharacterCard = (props) => {
+    return (
+        <div className='character-wrapper'>
+             <Char>{props.character[0].name}</Char> 
+             <Char>{props.character[1].name}</Char>
+             <Char>{props.character[2].name}</Char>
+             <Char>{props.character[3].name}</Char>
+             <Char>{props.character[4].name}</Char>
+             <Char>{props.character[5].name}</Char>
+        </div>
+    )
+}
+
+export default CharacterCard;
